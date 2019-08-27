8548e6f0c4e515088918ea1bb362c4b7a841a68c
Mextli Vazquez <mvazquez@MacBook-Pro-de-Mextli.local>
2019-08-27 02:27:49 -0500

    Updates

---
+++ precache-manifest.ecd9793055262fc7b3791768d7583563.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc1056d37eaa9f8cf1b0760606b40ac",
+    "url": "/descartes/index.html"
+  },
+  {
+    "revision": "d327cdcf14f3dd804ce0",
+    "url": "/descartes/static/css/main.84f06434.chunk.css"
+  },
+  {
+    "revision": "6f6c0f29478d92308e66",
+    "url": "/descartes/static/js/2.c6481815.chunk.js"
+  },
+  {
+    "revision": "d327cdcf14f3dd804ce0",
+    "url": "/descartes/static/js/main.56180d1c.chunk.js"
+  },
+  {
+    "revision": "371a53ace7f921a25894",
+    "url": "/descartes/static/js/runtime~main.827fc569.js"
+  }
+]);
